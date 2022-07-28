cd7d51c490cd8aede34b0c2813e580115876f58b
Max Base <maxbasecode@gmail.com>
2022-07-28 16:23:09 +0200

    clean and convert to es6

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
+// Const
+const blockSize = 25;
+const rows = 20;
+const cols = 20;
+
 // Board
-var blockSize = 25;
-var rows = 20;
-var cols = 20;
-var board;
-var context; 
+let board;
+let context;
+let gameOver = false;
 
 // Snake head
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
 
-var velocityX = 0;
-var velocityY = 0;
+let velocityX = 0;
+let velocityY = 0;
 
-var snakeBody = [];
+let snakeBody = [];
 
 // Food
-var foodX;
-var foodY;
-
-var gameOver = false;
+let foodX;
+let foodY;
 
 // Functions
-function update()
-{
+const update = () => {
     if (gameOver) return;
 
-    context.fillStyle="black";
+    context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle="red";
+    context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
     if (snakeX == foodX && snakeY == foodY) {
@@ -36,65 +36,55 @@ function update()
         placeFood();
     }
 
-    for (let i = snakeBody.length-1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i-1];
-    }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
-    }
+    for (let i = snakeBody.length-1; i > 0; i--) snakeBody[i] = snakeBody[i-1];
+    if (snakeBody.length) snakeBody[0] = [snakeX, snakeY];
 
-    context.fillStyle="lime";
+    context.fillStyle = "lime";
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
-    for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-    }
+    for (let i = 0; i < snakeBody.length; i++) context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
 
     // Game over conditions
     if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) gameOver = true;
-    for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) gameOver = true;
-    }
+    for (let i = 0; i < snakeBody.length; i++) if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) gameOver = true;
     if(gameOver) {
         alert("Game Over");
         location.reload();
     }
 }
 
-function changeDirection(e) {
+const changeDirection = (e) => {
     if (e.code == "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    }
-    else if (e.code == "ArrowDown" && velocityY != -1) {
+    } else if (e.code == "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    }
-    else if (e.code == "ArrowLeft" && velocityX != 1) {
+    } else if (e.code == "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    }
-    else if (e.code == "ArrowRight" && velocityX != -1) {
+    } else if (e.code == "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
 }
 
-function placeFood() {
-    //(0-1) * cols -> (0-19.9999) -> (0-19) * 25
+const placeFood = () => {
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * rows) * blockSize;
 }
 
-// Initialization
-window.onload = () => {
+const load = () => {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
-    context = board.getContext("2d"); //used for drawing on the board
+    context = board.getContext("2d");
 
     placeFood();
     document.addEventListener("keyup", changeDirection);
-    setInterval(update, 1000/10); //100 milliseconds
+    setInterval(update, 100);
 };
+
+// Initialization
+window.addEventListener("load", load);
