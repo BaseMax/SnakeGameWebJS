e1360d85fcbb4bd5bad1c4dc7d0b36dc3219788a
Max Base <maxbasecode@gmail.com>
2022-07-28 16:32:02 +0200

    change == to === and != to !==

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const update = () => {
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
-    if (snakeX == foodX && snakeY == foodY) {
+    if (snakeX === foodX && snakeY === foodY) {
         snakeBody.push([foodX, foodY]);
         placeFood();
     }
@@ -47,7 +47,7 @@ const update = () => {
 
     // Game over conditions
     if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) gameOver = true;
-    for (let i = 0; i < snakeBody.length; i++) if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) gameOver = true;
+    for (let i = 0; i < snakeBody.length; i++) if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]) gameOver = true;
     if(gameOver) {
         alert("Game Over");
         location.reload();
@@ -55,16 +55,16 @@ const update = () => {
 }
 
 const changeDirection = (e) => {
-    if (e.code == "ArrowUp" && velocityY != 1) {
+    if (e.code === "ArrowUp" && velocityY !== 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if (e.code == "ArrowDown" && velocityY != -1) {
+    } else if (e.code === "ArrowDown" && velocityY !== -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if (e.code == "ArrowLeft" && velocityX != 1) {
+    } else if (e.code === "ArrowLeft" && velocityX !== 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if (e.code == "ArrowRight" && velocityX != -1) {
+    } else if (e.code === "ArrowRight" && velocityX !== -1) {
         velocityX = 1;
         velocityY = 0;
     }
